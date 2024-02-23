1cbe8abd768b3ecdebef9491a4890fdd757dec25
Sim's Dev <simonetoa04@gmail.com>
2024-02-23 08:48:16 +0100

    petit ajout

--- src/Pack.java
+++ src/Pack.java
@@ -4,7 +4,9 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class Pack extends JFrame {
+
     public Pack(){
         JFrame frame = new JFrame("Menu");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
